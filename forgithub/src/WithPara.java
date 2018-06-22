d70b8dcc5ae5804e2206763086eb372c60064df2
兔子壳 <80938838@qq.com>
2018-06-22 15:53:50 +0800

    second edit from my IDEA. Change "爱慕课" to "慕课网".

--- forgithub/src/WithPara.java
+++ forgithub/src/WithPara.java
@@ -9,7 +9,7 @@ public class WithPara {
         public static void main(String[] args) {
 
             WithPara hello = new WithPara();
-            String welcome = hello.show("慕课网");
+            String welcome = hello.show("爱慕课");
             System.out.println(welcome);
         }
         private String show(String name){
